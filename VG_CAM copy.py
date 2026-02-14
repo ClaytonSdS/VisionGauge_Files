f17a22f3a0a9cea5f950088ee29e41fae846aeb4
Clayton Silva dos Santos <95229093+ClaytonSdS@users.noreply.github.com>
2026-02-14 10:55:05 -0300

    update: vg gifs

--- VG_CAM copy.py
+++ VG_CAM copy.py
@@ -4,10 +4,10 @@ from VisionGauge import VisionGauge
 model  = VisionGauge()
 
 camera = cv2.VideoCapture(0)
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+#camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+#camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-#cap = cv2.VideoCapture("http://192.168.2.117:8080/video")
+camera = cv2.VideoCapture("http://192.168.2.117:8080/video")
 
 model.predict_streaming(
     camera,
